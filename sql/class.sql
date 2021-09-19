00b5e84b56178b3af49688c8c0228c576e6750b0
Ong Jia Le <jiale.ong.2019@scis.smu.edu.sg>
2021-09-20 02:40:08 +0800

    chore: create class table creation sql

---
+++ sql/class.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS `spm_g7t4`DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `spm_g7t4`;
+
+DROP TABLE IF EXISTS `class`;
+CREATE TABLE IF NOT EXISTS `class` (
+    `class_id` int(4) NOT NULL,
+    `class_size` int(3) NOT NULL,
+    `trainer_email` varchar(50) DEFAULT NULL,
+    `course_id` char(7)NOT NULL,
+    
+    FOREIGN KEY (course_id) REFERENCES course(course_id),
+    FOREIGN KEY (trainer_email) REFERENCES trainer(email)
+) ;
+
+INSERT INTO `class` (`class_id`, `class_size`, `trainer_email`, `course_id`) 
+VALUES (1, 40, "jiale@smu.edu.sg", "REP1101");
+
+INSERT INTO `class` (`class_id`, `class_size`, `trainer_email`, `course_id`) 
+VALUES (2, 40, "swarna@smu.edu.sg", "REP1101");
+
+INSERT INTO `class` (`class_id`, `class_size`, `course_id`) 
+VALUES (1, 40, "REP2101");
