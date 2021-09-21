0fc6d975b92d40f2b68039e27e96a3f4ce949bd3
Ong Jia Le <jiale.ong.2019@scis.smu.edu.sg>
2021-09-21 11:40:07 +0800

    chore: update class table creation sql

--- sql/class.sql
+++ sql/class.sql
@@ -6,17 +6,21 @@ CREATE TABLE IF NOT EXISTS `class` (
     `class_id` int(4) NOT NULL,
     `class_size` int(3) NOT NULL,
     `trainer_email` varchar(50) DEFAULT NULL,
+    `start_date` date NOT NULL,
+    `end_date` date NOT NULL,
     `course_id` char(7)NOT NULL,
     
     FOREIGN KEY (course_id) REFERENCES course(course_id),
-    FOREIGN KEY (trainer_email) REFERENCES trainer(email)
+    FOREIGN KEY (trainer_email) REFERENCES trainer(email),
+    CHECK (end_date > start_date),
+    PRIMARY KEY (class_id, course_id)
 ) ;
 
-INSERT INTO `class` (`class_id`, `class_size`, `trainer_email`, `course_id`) 
-VALUES (1, 40, "jiale@smu.edu.sg", "REP1101");
+INSERT INTO `class` (`class_id`, `class_size`, `trainer_email`, `start_date`, `end_date`, `course_id`) 
+VALUES (1, 40, "jiale@smu.edu.sg", "2021-01-07 00:00:00", "2021-05-30 23:59:59", "REP1101");
 
-INSERT INTO `class` (`class_id`, `class_size`, `trainer_email`, `course_id`) 
-VALUES (2, 40, "swarna@smu.edu.sg", "REP1101");
+INSERT INTO `class` (`class_id`, `class_size`, `trainer_email`, `start_date`, `end_date`, `course_id`)  
+VALUES (2, 40, "swarna@smu.edu.sg", "2021-01-07 00:00:00", "2021-05-30 23:59:59", "REP1101");
 
-INSERT INTO `class` (`class_id`, `class_size`, `course_id`) 
-VALUES (1, 40, "REP2101");
+INSERT INTO `class` (`class_id`, `class_size`, `trainer_email`, `start_date`, `end_date`, `course_id`)  
+VALUES (1, 30, "swarna@smu.edu.sg", "2021-01-07 00:00:00", "2021-05-30 23:59:59", "REP2101");
