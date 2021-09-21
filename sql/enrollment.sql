a1c93a00770a891ef4c77410447dd0b137dc5ff1
Ong Jia Le <jiale.ong.2019@scis.smu.edu.sg>
2021-09-21 11:40:28 +0800

    chore: update enrollment table creation sql
    added enroller_email

--- sql/enrollment.sql
+++ sql/enrollment.sql
@@ -4,16 +4,17 @@ USE `spm_g7t4`;
 DROP TABLE IF EXISTS `enrollment`;
 CREATE TABLE IF NOT EXISTS `enrollment` (
     `learner_email` varchar(64) NOT NULL,
-    `enrollmentDate` date NOT NULL,
+    `enrollment_date` date NOT NULL,
     `course_id` char(7) NOT NULL,
     `class_id` int(4) NOT NULL,
+    `enroller_email` varchar(64) DEFAULT NULL,
 
     FOREIGN KEY (learner_email) REFERENCES learner(email),
     FOREIGN KEY (course_id, class_id) REFERENCES class(course_id, class_id)
 );
 
-INSERT INTO `enrollment` (`learner_email`, `enrollmentDate`, `course_id`, `class_id`) 
+INSERT INTO `enrollment` (`learner_email`, `enrollment_date`, `course_id`, `class_id`) 
 VALUES ("sean@smu.edu.sg", CURRENT_DATE(), "REP1101", 1);
 
-INSERT INTO `enrollment` (`learner_email`, `enrollmentDate`, `course_id`, `class_id`) 
+INSERT INTO `enrollment` (`learner_email`, `enrollment_date`, `course_id`, `class_id`) 
 VALUES ("niankai@smu.edu.sg", CURRENT_DATE(), "REP1101", 2);
