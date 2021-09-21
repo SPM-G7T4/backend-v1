2cd0d2530554ca31b19158d3485071b3059a4b67
Ong Jia Le <jiale.ong.2019@scis.smu.edu.sg>
2021-09-21 11:39:55 +0800

    chore: update course table creation sql

--- sql/course.sql
+++ sql/course.sql
@@ -5,13 +5,11 @@ DROP TABLE IF EXISTS `course`;
 CREATE TABLE IF NOT EXISTS `course` (
     `course_id` char(7) NOT NULL PRIMARY KEY,
     `course_name` varchar(64) NOT NULL,
-    `start_date` datetime NOT NULL,
-    `end_date` datetime NOT NULL,
     `created_date` timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO `course` (`course_id`, `course_name`, `start_date`, `end_date`) 
-VALUES ("REP1101", "General Repairs", "2021-01-07 00:00:00", "2021-05-30 23:59:59");
+INSERT INTO `course` (`course_id`, `course_name`) 
+VALUES ("REP1101", "General Repairs");
 
-INSERT INTO `course` (`course_id`, `course_name`, `start_date`, `end_date`) 
-VALUES ("REP2101", "Printing Coach Repairs", "2021-01-07 00:00:00", "2021-05-30 23:59:59");
+INSERT INTO `course` (`course_id`, `course_name`) 
+VALUES ("REP2101", "Printing Coach Repairs");
