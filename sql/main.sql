2aeaf238c94afe09fe39dace6abaee1cc68aa9d3
Ong Jia Le <jiale.ong.2019@scis.smu.edu.sg>
2021-09-29 21:39:55 +0800

    chore: change spelling of enrollment to enrolment
    also includes affected columns

--- sql/main.sql
+++ sql/main.sql
@@ -1,3 +1,4 @@
+DROP DATABASE `spm_g7t4`;
 CREATE DATABASE IF NOT EXISTS `spm_g7t4`DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `spm_g7t4`;
 
@@ -90,24 +91,24 @@ VALUES (1, 30, "swarna@smu.edu.sg", "2021-01-07 00:00:00", "2021-05-30 23:59:59"
 CREATE DATABASE IF NOT EXISTS `spm_g7t4`DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `spm_g7t4`;
 
-DROP TABLE IF EXISTS `enrollment`;
-CREATE TABLE IF NOT EXISTS `enrollment` (
+DROP TABLE IF EXISTS `enrolment`;
+CREATE TABLE IF NOT EXISTS `enrolment` (
     `learner_email` varchar(64) NOT NULL,
-    `enrollment_date` date NOT NULL,
+    `enrolment_date` date NOT NULL,
     `course_id` char(7) NOT NULL,
     `class_id` int(4) NOT NULL,
-    `hr_enroller_email` varchar(64) DEFAULT NULL,
+    `hr_enroler_email` varchar(64) DEFAULT NULL,
 
-    PRIMARY KEY (learner_email, course_id, class_id, enrollment_date),
+    PRIMARY KEY (learner_email, course_id, class_id, enrolment_date),
     FOREIGN KEY (learner_email) REFERENCES learner(email),
     FOREIGN KEY (course_id, class_id) REFERENCES class(course_id, class_id),
-    FOREIGN KEY (hr_enroller_email) REFERENCES hr(email)
+    FOREIGN KEY (hr_enroler_email) REFERENCES hr(email)
 );
 
-INSERT INTO `enrollment` (`learner_email`, `enrollment_date`, `course_id`, `class_id`) 
+INSERT INTO `enrolment` (`learner_email`, `enrolment_date`, `course_id`, `class_id`) 
 VALUES ("sean@smu.edu.sg", CURRENT_DATE(), "REP1101", 1);
 
-INSERT INTO `enrollment` (`learner_email`, `enrollment_date`, `course_id`, `class_id`) 
+INSERT INTO `enrolment` (`learner_email`, `enrolment_date`, `course_id`, `class_id`) 
 VALUES ("niankai@smu.edu.sg", CURRENT_DATE(), "REP1101", 2);
 
 -- Section
