3073298017c27b5f8960813d31b3d2ab64cf4fba
NKTeo <51158551+NKTeo@users.noreply.github.com>
2021-10-07 00:05:36 +0800

    chore: convert to "date" to "datetime" and convert JSON datetime to correct datetime format

--- application/course.py
+++ application/course.py
@@ -1,48 +1,6 @@
 from application.models import Course, Class, Prerequisite
 from flask import jsonify
 
-def view_all_courses_test():
-    try:
-        return jsonify(
-            {
-                "code": 200,
-                "message": "Success",
-                "data": {
-                    "courses": [
-                        {
-                            "course_id": "REP1101",
-                            "course_name": "Printer Concepts and Terminology",
-                            "created_datetime": "2021-03-20 10:00:00",
-                            "classes": [1,2,3],
-                            "prerequisites": []
-                        },
-                        {
-                            "course_id": "REP1201",
-                            "course_name": "Printer Operations",
-                            "created_datetime":"2021-03-20 10:00:00",
-                            "classes": [1,2,3,4,5],
-                            "prerequisites": []
-                        },
-                        {
-                            "course_id": "REP1301",
-                            "course_name": "Printer Startup Tutorial",
-                            "created_datetime":"2021-03-20 10:00:00",
-                            "classes": [1,2],
-                            "prerequisites": ["REP1101","REP1201"]
-                        }
-                    ]
-                }
-            }
-        )
-
-    except Exception as e:
-        return jsonify(
-            {
-                "code": 404,
-                "message": "There was an issue retrieving all courses. " + str(e)
-            }
-        )
-
 # Add more functions here 
 def view_all_courses():
     try :
@@ -67,7 +25,7 @@ def view_all_courses():
             courseJSON["prerequisites"] = prerequisiteList
 
             listOfCourses.append(courseJSON)
-            
+
         # print(listOfCourses)
         return jsonify(
             {
@@ -93,13 +51,11 @@ def view_course_details_test(course_id):
     "data": {
         "course_id": "REP1101",
         "course_name": "Printer Concepts and Terminology",
-        "sections": 4,
-        "videos": 4,
         "classes": [1,2,3,5,6],
-        "duration": "3.5 hours",
-        "description": "Briefly describes the relationship between printers, and their assigned lines, processes and statuses."
-
+        "description": "Briefly describes the relationship between printers, and their assigned lines, processes and statuses.",
+        "prerequisites": []
     }
+
 }
 
 def view_course_details(course_id):
--- application/models.py
+++ application/models.py
@@ -1,5 +1,6 @@
 """Data models."""
 from . import db # import db object created in __init__.py
+from datetime import datetime
 
 class Learner(db.Model):
     __tablename__ = 'learner'
@@ -59,18 +60,18 @@ class Course(db.Model):
     __tablename__ = 'course'
     course_id = db.Column(db.String(7), primary_key=True)
     course_name = db.Column(db.String(64), nullable=False)
-    created_date = db.Column(db.DateTime, nullable=True)
+    created_datetime = db.Column(db.DateTime, nullable=True)
 
-    def __init__(self, course_id, course_name, created_date):
+    def __init__(self, course_id, course_name, created_datetime):
         self.course_id = course_id
         self.course_name = course_name
-        self.created_date = created_date
+        self.created_datetime = created_datetime
 
     def json(self):
         return {
             "course_id": self.course_id,
             "course_name": self.course_name,
-            "created_date": self.created_date
+            "created_datetime": self.created_datetime.strftime("%Y-%m-%d %H:%M:%S")
         }
 
 class Class(db.Model):
@@ -95,8 +96,8 @@ class Class(db.Model):
             "class_id": self.class_id,
             "class_size": self.class_size,
             "trainer_email": self.trainer_email,
-            "start_datetime": self.start_datetime,
-            "end_datetime": self.end_datetime,
+            "start_datetime": self.start_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+            "end_datetime": self.end_datetime.strftime("%Y-%m-%d %H:%M:%S"),
             "course_id": self.course_id
         }
 
@@ -104,16 +105,16 @@ class Class(db.Model):
 class Enrolment(db.Model):
     __tablename__ = 'enrolment'
     learner_email = db.Column(db.String(64), db.ForeignKey('learner.email'), primary_key=True)
-    enrolment_date = db.Column(db.DateTime, nullable=False)
+    enrolment_datetime = db.Column(db.DateTime, nullable=False)
     course_id = db.Column(db.String(7), db.ForeignKey('course.id'), primary_key=True)
     class_id = db.Column(db.Integer, db.ForeignKey('class.id'), primary_key=True)
     class_start_datetime = db.Column(db.DateTime, db.ForeignKey('class.start_datetime'), primary_key=True)
     hr_enroler_email = db.Column(db.String(64), db.ForeignKey('hr.email'), nullable=True)
     approver_email = db.Column(db.String(64), db.ForeignKey('hr.email'), nullable=False)
 
-    def __init__(self, learner_email, enrolment_date, course_id, class_id, class_start_datetime, hr_enroler_email):
+    def __init__(self, learner_email, enrolment_datetime, course_id, class_id, class_start_datetime, hr_enroler_email):
         self.learner_email = learner_email
-        self.enrolment_date = enrolment_date
+        self.enrolment_datetime = enrolment_datetime
         self.course_id = course_id
         self.class_id = class_id
         self.class_start_datetime = class_start_datetime
@@ -123,10 +124,10 @@ class Enrolment(db.Model):
     def json(self):
         return {
             "learner_email": self.learner_email,
-            "enrolment_date": self.enrolment_date,
+            "enrolment_datetime": self.enrolment_datetime.strftime("%Y-%m-%d %H:%M:%S"),
             "course_id": self.course_id,
             "class_id": self.class_id,
-            "class_start_datetime": self.class_start_datetime,
+            "class_start_datetime": self.class_start_datetime.strftime("%Y-%m-%d %H:%M:%S"),
             "hr_enroler_email": self.hr_enroler_email
         }
 
@@ -134,16 +135,16 @@ class Prerequisite(db.Model):
     __tablename__ = 'prerequisite'
     prerequisite_id = db.Column(db.String(7), db.ForeignKey('course.course_id'), primary_key=True)
     postrequisite_id = db.Column(db.String(7), db.ForeignKey('course.course_id'), primary_key=True)
-    created_date = db.Column(db.DateTime, nullable=True)
+    created_datetime = db.Column(db.DateTime, nullable=True)
 
-    def __init__(self, prerequisite_id, postrequisite_id, created_date):
+    def __init__(self, prerequisite_id, postrequisite_id, created_datetime):
         self.prerequisite_id = prerequisite_id
         self.postrequisite_id = postrequisite_id
-        self.created_date = created_date
+        self.created_datetime = created_datetime
     
     def json(self):
         return {
             "prerequisite_id": self.prerequisite_id,
             "postrequisite_id": self.postrequisite_id,
-            "created_date": self.created_date
+            "created_datetime": self.created_datetime.strftime("%Y-%m-%d %H:%M:%S")
         }
