6d12662e3a6e72b04192765aec0be2f00e1945b3
NKTeo <51158551+NKTeo@users.noreply.github.com>
2021-10-10 16:51:44 +0800

    Chore/create backend query db (#6)
    7 callable API:
    
    GET - localhost:5000/learners
    GET - localhost:5000/hr
    GET - localhost:5000/trainers
    GET - localhost:5000/courses/<course_id>
    POST - localhost:5000/enrolments/create
    POST - localhost:5000/enrolments/view

--- application/course.py
+++ application/course.py
@@ -1,32 +1,65 @@
-from application.models import Learner
+from application.models import Course, Class, Prerequisite
 from flask import jsonify
 
-def view_all_courses():
-    return jsonify(
-        {
-            "code" : 200,
-            "message": "Success",
-            "data" :{
-                "courses" : [
-                    {
-                        "courseID": "REP2101",
-                        "courseName": "Printer Coach Repairs",
-                        "courseStartDate": "2021-03-20 10:00:00",
-                        "courseEndDate": "2021-09-01 11:59:00",
-                        "classes": ["C1","C2"],
-                        "prerequisites": ["REP1101","REP1201"]
-                    },
-                    {
-                        "courseID": "REP1101",
-                        "courseName": "General Repairs",
-                        "courseStartDate": "2021-03-20 10:00:00",
-                        "courseEndDate": "2021-09-01 11:59:00",
-                        "classes": ["C1","C2","C3","C4","C5"],
-                        "prerequisites": []
-                    }
-                ]
-            }
-        }
-    )
-
 # Add more functions here 
+def view_all_courses():
+    try :
+        listOfCourses = []
+        dbCourseList = Course.query.all()
+        for course in dbCourseList:
+            courseJSON = course.json()
+            classList = []
+            prerequisiteList = []
+
+            dbClassList = Class.query.filter_by(course_id = courseJSON["course_id"]).all()
+            for c in dbClassList:
+                classList.append(c.json()["class_id"])
+
+            courseJSON["classes"] = classList
+            
+
+            dbPrerequisite = Prerequisite.query.filter_by(postrequisite_id = courseJSON["course_id"]).all()
+            for p in dbPrerequisite:
+                prerequisiteList.append(p.json()["prerequisite_id"])
+            
+            courseJSON["prerequisites"] = prerequisiteList
+
+            listOfCourses.append(courseJSON)
+
+        # print(listOfCourses)
+        return jsonify(
+            {
+                "code": 200,
+                "message": "Success",
+                "data": {
+                    "courses": listOfCourses
+                }
+            }
+        )
+
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "There was an issue retrieving all courses. " + str(e)
+            }
+        )
+
+def view_course_details_test(course_id):
+    return {
+    "code": 200, 
+    "data": {
+        "course_id": "REP1101",
+        "course_name": "Printer Concepts and Terminology",
+        "classes": [1,2,3,5,6],
+        "description": "Briefly describes the relationship between printers, and their assigned lines, processes and statuses.",
+        "prerequisites": []
+    }
+
+}
+
+def view_course_details(course_id):
+    return {
+        "code" : 500,
+        "message" : "work in progress"
+        }
--- application/routes.py
+++ application/routes.py
@@ -2,35 +2,62 @@ from flask import render_template
 from flask import current_app as app
 
 # Routes to files
-from .course import view_all_courses
+
+# learner
 from .learner import view_all_learners
+# hr
 from .hr import view_all_hr
+# trainer
 from .trainer import view_all_trainers
 
+# course
+from .course import view_all_courses
+from .course import view_course_details_test
+
+# enrolment
+from .enrolment import create_enrolment_test
+from .enrolment import view_enrolment_test
+
+
+
+# Endpoints
 
 @app.route("/", methods=['GET'])
 def hello_world():
     return "<h1>Greetings from SPM-G7T4</h1>"
 
 # learner
-@app.route("/view_all_learners", methods=['GET'])
+@app.route("/learners", methods=['GET'])
 def return_all_learners():
     return view_all_learners()
 
 # hr
-@app.route("/view_all_hr", methods=['GET'])
+@app.route("/hr", methods=['GET'])
 def return_all_hr():
     return view_all_hr()
 
 # trainer
-@app.route("/view_all_trainers", methods=['GET'])
+@app.route("/trainers", methods=['GET'])
 def return_all_trainers():
     return view_all_trainers()
 
 # course
-@app.route("/view_all_courses", methods=['GET'])
+@app.route("/courses", methods=['GET'])
 def return_all_courses():
     return view_all_courses()
+
+@app.route("/courses/<course_id>", methods=['GET'])
+def return_course_details(course_id):
+    return view_course_details_test(course_id)
+
+# enrolment
+@app.route("/enrolments/create", methods=['POST'])
+def return_enrolment_creation_status():
+    return create_enrolment_test()
+
+@app.route("/enrolments/view", methods=['POST'])
+def return_all_enrolment():
+    return view_enrolment_test()
 
 @app.errorhandler(404)
 def page_not_found(error):
