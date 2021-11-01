aa2041b4bb2e3631aeee8b045817915da45947da
Ong Jia Le <jiale.ong.2019@scis.smu.edu.sg>
2021-11-01 19:14:39 +0800

    chore: update learner sql

--- sql/learner.sql
+++ sql/learner.sql
@@ -5,11 +5,13 @@ DROP TABLE IF EXISTS `learner`;
 CREATE TABLE IF NOT EXISTS `learner` (
     `email` varchar(64) NOT NULL PRIMARY KEY,
     `name` varchar(64) NOT NULL,
-    `password` varchar(32) NOT NULL
+    `password` varchar(32) NOT NULL,
+    `designation` varchar(64) NOT NULL,
+    `department` varchar(64) NOT NULL
 );
 
-INSERT INTO `learner` (`email`, `name`, `password`) 
-VALUES ("niankai@smu.edu.sg", "niankai", "123");
+INSERT INTO `learner` (`email`, `name`, `password`, `designation`, `department`) 
+VALUES ("niankai@smu.edu.sg", "Niankai", "123", "Junior systems engineer", "Engineering");
 
-INSERT INTO `learner` (`email`, `name`, `password`) 
-VALUES ("sean@smu.edu.sg", "sean", "123");
+INSERT INTO `learner` (`email`, `name`, `password`, `designation`, `department`) 
+VALUES ("sean@smu.edu.sg", "Sean", "123", "Junior electrical engineer", "Engineering");
